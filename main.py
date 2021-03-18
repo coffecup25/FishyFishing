e44923c44363d1d2e226e9308620d56f7094615a
Arian Hanifi <counterstrike1337@hotmail.com>
2021-03-18 20:52:09 +0100

    add print mouse position function

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import pyautogui as pag
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 while True:
     img = pag.screenshot()
     data = np.array(img)
     plt.imshow(data, interpolation='nearest')
     plt.show()
+
+
+def start_up():
+    pag.FAILSAFE = True
+
+    print("Starting up", end=" ")
+    for i in range(10):
+        print("#", end=" ")
+        time.sleep(1)
+ 
+    print("Ready")
+
+
+def main():
+    start_up()
+
+
+def mouse_position(duration=10):
+    for i in range(duration):
+        print(pag.position())
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
